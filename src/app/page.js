dd4c3ab94d8ab8a0e7578bfc6f8d473d7d0ff637
mersimoski <m.mersimoski1999@gmail.com>
2024-05-08 21:20:05 +0200

    chore: Add "main" class to the container in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { Grid } from "@mui/material";
 
 export default function Home() {
   return (
-    <Grid container sx={{ py: "200px" }}>
+    <Grid container sx={{ py: "200px" }} className="main">
       content
     </Grid>
   );
