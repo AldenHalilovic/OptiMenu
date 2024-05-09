97d829abbbd73afbe761191ba9d0ad319750a56b
Alden Halilovic <Aldehalilovic@gmail.com>
2024-05-09 17:22:48 +0200

    refactor: Fix import path for HeroContentThree component in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import HeroSection from "@/components/home/homeComponents/hero";
 import { Grid } from "@mui/material";
-import HeroContentThree from "@/components/Header/homeComponents/hero/components/heroContentThree";
+import HeroContentThree from "@/components/home/homeComponents/hero/components/heroContentThree";
 
 export default function Home() {
   return (
